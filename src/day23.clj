f15c1154ca4930a6dd16c490d9741094e0d9dd0e
Average-user <lucaspolymeris@protonmail.com>
2020-12-23 16:53:03 -0300

    changed maps to vectors in day23

--- src/day23.clj
+++ src/day23.clj
@@ -1,29 +1,33 @@
-(ns day23)
+(ns day23
+  (:require [clojure.string :refer [trim]]))
 
+(def input (map (comp read-string str) (trim (slurp "../inputs/day23.txt"))))
+                        
 (defn f [n i] (if (zero? i) n i))
+(defn g  [v i] (or (v i) (inc i)))
 
-(defn build [xs]
-  (conj (into {} (map vector xs (rest xs))) [(last xs) (first xs)]))
+(defn build [n xs]
+  (-> (reduce (fn [v [x y]] (assoc v x y))
+              (vec (repeat (inc n) nil))
+              (map vector xs (rest xs)))
+      (assoc (last xs) (first xs))))
 
 (defn game-step [n xs c]
-  (let [g          (fn [m i] (or (m i) (inc i)))
-        [x1 x2 x3] (list (g xs c) (g xs (g xs c)) (g xs (g xs (g xs c))))
+  (let [[x1 x2 x3] (list (g xs c) (g xs (g xs c)) (g xs (g xs (g xs c))))
         d          (first (filter (complement #{x1 x2 x3})
                                   (next (iterate #(f n (dec %)) c))))]
     (assoc xs d x1 c (g xs x3) x3 (g xs d))))
 
 (defn game [n xs c]
-  (iterate (fn [[xs c]] (let [ys (game-step n xs c)] [ys (ys c)]))
-           [xs c]))
+  (iterate (fn [[xs c]] (let [ys (game-step n xs c)] [ys (ys c)])) [xs c]))
                             
 (defn main- []
-  (let [in (map (comp read-string str) (filter (set "0123456789")
-                                               (slurp "../inputs/day23.txt")))
-        xs  (build in)
-        r   (first (nth (game (count in) xs (first in)) 100))
-        xs' (assoc xs (last in) (inc (count in)) 1000000 (first in))
-        r'  (first (nth (game 1000000 xs' (first in)) 10000000))]
-    [(read-string (apply str (take (dec (count in)) (next (iterate r 1)))))
+  (let [xs  (build (count input) input)
+        r   (first (nth (game (count input) xs (first input)) 100))
+        xs' (assoc (build 1000000 input)
+                   (last input) (inc (count input)) 1000000 (first input))
+        r'  (first (nth (game 1000000 xs' (first input)) 10000000))]
+    [(read-string (apply str (take (dec (count input)) (next (iterate r 1)))))
      (* (r' 1) (r' (r' 1)))]))
 
 (time (print (main-)))
